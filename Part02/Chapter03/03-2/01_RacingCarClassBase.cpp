c4bf5d9363dbd3124fec339116def7ce54c2f6df
LSJ <sneak7581@wku.ac.kr>
2020-12-27 14:20:31 +0900

    03_2 예제 01 클래스 기반 코딩

--- Part02/Chapter03/03-2/01_RacingCarClassBase.cpp
+++ Part02/Chapter03/03-2/01_RacingCarClassBase.cpp
@@ -6,11 +6,8 @@ namespace CAR_CONST
 {
     enum
     {
-        ID_LEN = 20,
-        MAX_SPD = 200,
-        FUEL_STEP = 2,
-        ACC_STEP = 10,
-        BRK_STEP = 10
+        ID_LEN = 20, MAX_SPD = 200, FUEL_STEP = 2,
+        ACC_STEP = 10, BRK_STEP = 10
     };
 }
 
@@ -20,7 +17,6 @@ private:
     char gamerID[CAR_CONST::ID_LEN];
     int fuelGauge;
     int curSpeed;
-
 public:
     void InitMembers(char *ID, int fuel);
     void ShowCarState();
@@ -33,35 +29,38 @@ void Car::InitMembers(char *ID, int fuel)
     strcpy(gamerID, ID);
     fuelGauge = fuel;
     curSpeed = 0;
-};
+}
+
 void Car::ShowCarState()
 {
     cout << "소유자 ID : " << gamerID << endl;
     cout << "연료량 : " << fuelGauge << "%" << endl;
-    cout << "현재속도 : " << curSpeed << "km/s" << endl
-         << endl;
+    cout << "현재속도 : " << curSpeed << "km/h" << endl << endl; 
 }
+
 void Car::Accel()
 {
-    if (fuelGauge <= 0)
+    if(fuelGauge <= 0)
         return;
-    else
-        fuelGauge -= CAR_CONST::FUEL_STEP;
+    else fuelGauge -= CAR_CONST::FUEL_STEP;
 
-    if ((curSpeed + CAR_CONST::ACC_STEP) >= CAR_CONST::MAX_SPD)
+    if(curSpeed + CAR_CONST::ACC_STEP >= CAR_CONST::MAX_SPD)
     {
         curSpeed = CAR_CONST::MAX_SPD;
         return;
     }
+    
     curSpeed += CAR_CONST::ACC_STEP;
 }
+
 void Car::Break()
 {
-    if (curSpeed < CAR_CONST::BRK_STEP)
+    if(curSpeed < CAR_CONST::BRK_STEP)
     {
         curSpeed = 0;
         return;
     }
+
     curSpeed -= CAR_CONST::BRK_STEP;
 }
 
@@ -75,5 +74,6 @@ int main(void)
     run99.ShowCarState();
     run99.Break();
     run99.ShowCarState();
+
     return 0;
 }
