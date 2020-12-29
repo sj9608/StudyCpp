58be2812b8ec3ef00f46185d00b537934b19ed89
LSJ <sneak7581@wku.ac.kr>
2020-12-29 17:49:41 +0900

    04-3 예제 04 참조자, const 멤버 변수 초기화

---
+++ Part02/Chapter04/04-3/04_ReferenceMember.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class AAA
+{
+public:
+    AAA()
+    {
+        cout << "empty object" << endl;
+    }
+    void ShowYourName() const
+    {
+        cout << "I'm class AAA" << endl;
+    }
+};
+
+class BBB
+{
+private:
+    AAA &ref;
+    const int &num;
+public:
+    BBB(AAA &r, const int &n)
+        : ref(r), num(n)
+    {
+
+    }
+    void ShowYourName() const
+    {
+        cout << "I'm Class BBB" << endl;
+        cout << "and I ref num : " << num << endl;
+    }
+};
+
+int main(void)
+{
+    AAA obj1;
+    BBB obj2(obj1, 20);
+    obj2.ShowYourName();
+
+    return 0;
+}
