a2a9cd8a79aa7de11c28f28d4921ad32dc31a505
LSJ <sneak7581@wku.ac.kr>
2020-12-31 17:38:45 +0900

    05-1 복사 생성자 , 디폴트 복사 생성자, explicit키워드

--- Part02/Chapter05/05-1/01_ClassInit.cpp
+++ Part02/Chapter05/05-1/01_ClassInit.cpp
@@ -7,6 +7,11 @@ private:
     int num1, num2;
 public:
     SoSimple(int n1, int n2) : num1(n1), num2(n2) { }
+    SoSimple(SoSimple &copy) : num1(copy.num1), num2(copy.num2) 
+    {
+        cout << "Called SoSimple(SoSimple &copy)" << endl;
+    } 
+
     void ShowSimpleData()
     {
         cout << num1 << ", " << num2 << endl;
@@ -16,7 +21,8 @@ public:
 int main(void)
 {
     SoSimple sim1(15, 20);
-    SoSimple sim2 = sim1;
+    cout << "생성 및 초기화 직전" << endl;
+    SoSimple sim2 = sim1; // SoSimple sim2(sim1) 이와 같이 작동하는지 확인 하기 위한 코드
 
     sim2.ShowSimpleData();
     return 0;
