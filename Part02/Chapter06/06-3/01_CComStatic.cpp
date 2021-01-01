7e7f01372b3026fd9d50f2953a7be29255989baa
LSJ <sneak7581@wku.ac.kr>
2021-01-01 17:02:53 +0900

    06-3 예제 01 C언어 에서의 static

---
+++ Part02/Chapter06/06-3/01_CComStatic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void Counter()
+{
+    static int cnt;
+    cnt++;
+    cout << "Current cnt is : " << cnt << endl;
+}
+
+int main(void)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Counter();
+    }
+
+    return 0;
+}
