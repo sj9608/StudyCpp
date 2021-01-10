b4527e255ba34282d6f969a7f26977ea2d5e6ecd
LSJ <sneak7581@wku.ac.kr>
2021-01-10 21:48:44 +0900

    11-1 예제 02 디폴트 대입 연산자의 문제점 (동적할당 메모리누수)

---
+++ Part04/Chapter11/11-1/02_AssignShallowCopyError.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Person
+{
+private:
+    char * name;
+    int age;
+public:
+    Person(char *mychar, int myage)
+    {
+        int len = strlen(mychar) + 1;
+        name = new char[len];
+        strcpy(name, mychar);
+        age = myage;
+    }
+    void ShowPersonData() const
+    {
+        cout << "이름 : " << name << endl;
+        cout << "나이 : " << age << endl;
+    }
+
+    ~Person()
+    {
+        delete []name;
+        cout << "called Destructor!" << endl;
+    }
+};
+
+int main(void)
+{
+    Person man1("Lee dong woo", 29);
+    Person man2("Yoon ji yul", 22);
+
+    man2 = man1;
+
+    man1.ShowPersonData();
+    man2.ShowPersonData();
+    return 0;
+}
