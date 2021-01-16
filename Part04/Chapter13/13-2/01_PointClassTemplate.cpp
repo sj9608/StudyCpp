b4058e778c8860ca23ec395e19303b1433d62910
LSJ <sneak7581@wku.ac.kr>
2021-01-16 18:21:13 +0900

    11-2 예제 01, 02 Point 클래스 템플릿

---
+++ Part04/Chapter13/13-2/01_PointClassTemplate.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Point
+{
+private:
+    T xpos, ypos;
+public:
+    Point(T x, T y) : xpos(x), ypos(y)
+    { }
+    void ShowPosition() const
+    {
+        cout << '[' << xpos << ", " << ypos << ']' << endl;
+    }
+};
+
+int main(void)
+{
+    Point<int> pos1(3, 4);
+    pos1.ShowPosition();
+
+    Point<double> pos2(2.4, 3.6);
+    pos2.ShowPosition();
+
+    Point<char> pos3('P', 'F');
+    pos3.ShowPosition();
+    
+    return 0;
+}
